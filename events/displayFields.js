1efad883968ce4aea8f036a67c5b9daaedac847f
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-07-08 15:40:19 -0400

    Campos Ocultos - Display

--- events/displayFields.js
+++ events/displayFields.js
@@ -1,10 +1,10 @@
 function displayFields(form,customHTML){
-    var Now_State = parseInt(getValue("WKNumState"));
+    var Now = parseInt(getValue("WKNumState"));
     var NumProcesso = getValue("WKNumProces");
     var usuario = getValue("WKUser");
     var data = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-    switch(Now_State){
+    switch(Now){
         case 0:
             form.setValue("cmb_NomeSolicitante",usuario);
             form.setValue("dt_DataSolicit",data.format(new Date()));
@@ -20,4 +20,26 @@ function displayFields(form,customHTML){
 
         default:
     }
+
+    if (Now == 0 || Now == 4 || Now == 7 || Now == 12 || Now == 15 || Now == 31 || Now == 33 || Now == 35 || Now == 68 || Now == 70 || Now == 74 || Now == 78 || Now == 80) {
+        form.setVisibleById("disp", false);
+        form.setVisibleById("aprov", false);
+        form.setVisibleById("stats", false);
+        }
+
+    if (Now == 27) {
+        form.setVisibleById("aprov", false);
+        form.setVisibleById("stats", false);
+    }
+
+    if (Now == 57) {
+        form.setVisibleById("disp", false);
+        form.setVisibleById("stats", false);
+    }
+
+    if (Now == 64) {
+        form.setVisibleById("disp", false);
+        form.setVisibleById("aprov", false);
+    }
+
  }
